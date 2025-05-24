cce7f3e413e2235ede305fb6a4bc9a1c2bfe1a62
UgrynBohdan <ugrynbogdan7@gmail.com>
2025-05-24 22:05:14 +0300

    Змінив розташування папки з даними для моделі, трохи доробив структуру проєкту

---
+++ model/res/random_forest_models/v0/run.py
@@ -0,0 +1,18 @@
+['competition_id', 'season', 'round', 'home_club_id', 'away_club_id',
+        'home_club_position', 'away_club_position', 'home_club_manager_name',
+        'away_club_manager_name', 'stadium', 'attendance', 'referee',
+        'domestic_competition_id_home_club', 'squad_size_home_club',
+        'average_age_home_club', 'foreigners_number_home_club',
+        'national_team_players_home_club', 'stadium_name_home_club',
+        'stadium_seats_home_club', 'last_season_home_club',
+        'domestic_competition_id_away_club', 'squad_size_away_club',
+        'average_age_away_club', 'foreigners_number_away_club',
+        'national_team_players_away_club', 'stadium_name_away_club',
+        'stadium_seats_away_club', 'last_season_away_club', 'country_id',
+        'is_major_national_league', 'year', 'month', 'day', 'dayofweek',
+        'home_club_formation_code', 'away_club_formation_code',
+        'net_transfer_record_home_club_num',
+        'net_transfer_record_away_club_num']
+
+['home_club_goals', 'away_club_goals']
+
